c24b448b9e2d95f2f16d64a05becf95a07545b0f
bismillah416 <142307313+bismillah416@users.noreply.github.com>
2023-09-22 22:19:55 +0700

    Create 468x60.js

---
+++ 468x60.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '4d495df6cba56ddb2974321a3296a9cb',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="https://cdn.jsdelivr.net/gh/bismillah416/blogging@main/invoke468x60.js"></scr' + 'ipt>');
