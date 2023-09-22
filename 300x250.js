2f1807acec3759df1f6027c661ed593f21ddd9ee
bismillah416 <142307313+bismillah416@users.noreply.github.com>
2023-09-22 22:24:27 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '3fe25671d3d979f9c8883e4696a9b79f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="https://cdn.jsdelivr.net/gh/bismillah416/blogging@main/invoke300x250.js"></scr' + 'ipt>');
