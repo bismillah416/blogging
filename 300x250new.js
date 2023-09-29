585291522c76be29ff6c5a755089132b4aa700d6
bismillah416 <142307313+bismillah416@users.noreply.github.com>
2023-09-29 17:20:48 +0700

    Create 300x250new.js

---
+++ 300x250new.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '3fe25671d3d979f9c8883e4696a9b79f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//terminusbedsexchanged.com/3fe25671d3d979f9c8883e4696a9b79f/invoke.js"></scr' + 'ipt>');
