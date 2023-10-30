1f6c26d1702e8eccdc1eca0dbf078525b766c4af
bismillah416 <142307313+bismillah416@users.noreply.github.com>
2023-10-30 08:45:56 +0700

    Create 300x250blog.js

---
+++ 300x250blog.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f8cdcc77e3b080ae36e9c5d66c70a78b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//terminusbedsexchanged.com/f8cdcc77e3b080ae36e9c5d66c70a78b/invoke.js"></scr' + 'ipt>');
