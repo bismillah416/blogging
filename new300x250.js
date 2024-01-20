e5f71a5481a0981ab730a597754e83fe981b0f2d
bismillah416 <142307313+bismillah416@users.noreply.github.com>
2024-01-21 06:11:11 +0700

    Create new300x250.js

---
+++ new300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'ddad80e7cc91530c73a997f1b1fb5a79',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//terminusbedsexchanged.com/ddad80e7cc91530c73a997f1b1fb5a79/invoke.js"></scr' + 'ipt>');
