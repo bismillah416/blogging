0855a0ed33f6078fdc9749525d3aa66234f19f74
bismillah416 <142307313+bismillah416@users.noreply.github.com>
2024-04-05 06:35:59 +0700

    Create 728x90blog.js

---
+++ 728x90blog.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '45acc3909a7f55c6c984dfd56bfb7e05',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//theologicalsnowshorter.com/45acc3909a7f55c6c984dfd56bfb7e05/invoke.js"></scr' + 'ipt>');
