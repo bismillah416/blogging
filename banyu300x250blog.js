b93e482351ed943d88b9a3ff15911a2bab4d54aa
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-04-29 20:19:17 +0700

    Create banyu300x250blog.js

---
+++ banyu300x250blog.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : 'c9132b1546f661fc9db98025b3a7cd47',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/c9132b1546f661fc9db98025b3a7cd47/invoke.js"></scr' + 'ipt>');
