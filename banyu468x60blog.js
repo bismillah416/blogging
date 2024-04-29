8088685027f7b241c9a775db250451767dba9e5b
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-04-29 20:55:49 +0700

    Create banyu468x60blog.js

---
+++ banyu468x60blog.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '596b0ce49e62678f50d8bea676e75766',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/596b0ce49e62678f50d8bea676e75766/invoke.js"></scr' + 'ipt>');
