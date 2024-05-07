d63b38c256be8ac6c9e71d4aef9782c79727615c
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-05-07 23:57:06 +0700

    Create 300x250.js

---
+++ banyu/300x250.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '454d01736769285d026ccf91f9204018',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/454d01736769285d026ccf91f9204018/invoke.js"></scr' + 'ipt>');
