e8b8e0e064dd30a285d05891d278a4621084e47d
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-05-08 00:58:54 +0700

    Create 468x60.js

---
+++ banyu/468x60.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '5eaaf769e85aa29a5d68794a73d17e45',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/5eaaf769e85aa29a5d68794a73d17e45/invoke.js"></scr' + 'ipt>');
