c0f6641750b0a4642944de4feb0917b6bb470879
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-05-10 07:36:51 +0700

    Create 468x60.js

---
+++ master/468x60.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '1eb7aed25e93aaed06da69d7655c3f48',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/1eb7aed25e93aaed06da69d7655c3f48/invoke.js"></scr' + 'ipt>');
