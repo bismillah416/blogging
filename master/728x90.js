a5080b1a8944299657bf750bacf7432cdba3d74d
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-05-10 07:34:21 +0700

    Create 728x90.js

---
+++ master/728x90.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : 'f08dba0ab51e2a5ce07c5106137f17ba',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f08dba0ab51e2a5ce07c5106137f17ba/invoke.js"></scr' + 'ipt>');
