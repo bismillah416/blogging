e635ada1918c2dca4986978174d1723fca6c9156
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-05-12 21:22:35 +0700

    Create 300x250.js

---
+++ gan/300x250.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '25b75bff28ef761674ee640d55bec586',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/25b75bff28ef761674ee640d55bec586/invoke.js"></scr' + 'ipt>');
