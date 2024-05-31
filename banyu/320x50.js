f4dbf3c753266c515646efb8c085986b0f12ca5b
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-05-31 21:31:37 +0700

    Create 320x50.js

---
+++ banyu/320x50.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : 'cf384d4330035aab47721760f826dad3',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/cf384d4330035aab47721760f826dad3/invoke.js"></scr' + 'ipt>');
