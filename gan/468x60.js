8c5063286918ea1c36c7d788f67b3b51fde7ae38
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-10 16:31:01 +0700

    Update 468x60.js

--- gan/468x60.js
+++ gan/468x60.js
@@ -1,9 +1,9 @@
-atOptions = {
-		'key' : '9776c54c2922819b1af7dc368150eaf8',
+	atOptions = {
+		'key' : '9c6a21584bb193c00278ee676ae60644',
 		'format' : 'iframe',
 		'height' : 60,
 		'width' : 468,
 		'params' : {}
 	};
 
-document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/9776c54c2922819b1af7dc368150eaf8/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/9c6a21584bb193c00278ee676ae60644/invoke.js"></scr' + 'ipt>');
