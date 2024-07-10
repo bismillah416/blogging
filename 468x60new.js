6a8e0a349303c76cd62a2b99a73873810be12f03
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-10 16:26:12 +0700

    Create 468x60new.js

---
+++ 468x60new.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9c6a21584bb193c00278ee676ae60644',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/9c6a21584bb193c00278ee676ae60644/invoke.js"></scr' + 'ipt>');
