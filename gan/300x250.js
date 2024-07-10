13695101bac7f59e9e425d47da058917f28ace07
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-10 15:48:10 +0700

    Create 300x250.js

---
+++ gan/300x250.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : 'c6e6e50379b5614f832919abf4332f4f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/c6e6e50379b5614f832919abf4332f4f/invoke.js"></scr' + 'ipt>');
