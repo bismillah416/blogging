0f6afe781d7430d1d8b4d4f5de44578bfeabea1d
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-10 16:40:30 +0700

    Update 728x90.js

--- gan/728x90.js
+++ gan/728x90.js
@@ -1,9 +1,9 @@
 atOptions = {
-		'key' : 'ce080dce23ffbda26c4d79fee66118c8',
+		'key' : '28ea02213554b7e34162e9f5142506eb',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
 
-document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/ce080dce23ffbda26c4d79fee66118c8/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/28ea02213554b7e34162e9f5142506eb/invoke.js"></scr' + 'ipt>');
