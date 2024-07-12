1a1cac6acd0d3c6ab04ad8584fc0fab16e66b945
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-12 21:20:46 +0700

    Update socialbar.js

--- gan/socialbar.js
+++ gan/socialbar.js
@@ -1,1 +1,3 @@
 document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/d3/e5/a9/d3e5a9bb1f13c778562b21557904e784.js"></scr' + 'ipt>');
+
+document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/9a/3b/67/9a3b6748d0bc84e71242ef1d017831bd.js"></scr' + 'ipt>');
