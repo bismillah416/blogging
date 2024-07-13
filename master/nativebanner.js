d02fe758da7b8124dff6fb765a03b1580c47e890
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-13 08:09:15 +0700

    Create nativebanner.js

---
+++ master/nativebanner.js
@@ -0,0 +1,2 @@
+document.write('<script async="async" data-cfasync="false" src="//perilastronaut.com/f38c32ea2d09d8593a2f566f07996054/invoke.js"></script>');
+document.write('<div id="container-f38c32ea2d09d8593a2f566f07996054"></div>');
