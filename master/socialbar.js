5653dea709b1a7cb497105ad80e59b5a0be9a167
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-13 07:58:31 +0700

    Update socialbar.js

--- master/socialbar.js
+++ master/socialbar.js
@@ -1,1 +1,3 @@
 document.write('<scr' + 'ipt type="text/javascript" src="//pl23275115.highcpmgate.com/fe/0d/a5/fe0da5b25e979619156472b2fb697713.js"></scr' + 'ipt>');
+
+document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/d1/e5/9f/d1e59ffa253c83693d24baebfee6b2f5.js"></scr' + 'ipt>');
