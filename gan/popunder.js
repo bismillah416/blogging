6e19e44aa10952a4d77379c9ad2d643c7da15a9a
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-07-21 09:02:26 +0700

    Update popunder.js

--- gan/popunder.js
+++ gan/popunder.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/9a/3b/67/9a3b6748d0bc84e71242ef1d017831bd.js"></scr' + 'ipt>');
+// document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/9a/3b/67/9a3b6748d0bc84e71242ef1d017831bd.js"></scr' + 'ipt>');
