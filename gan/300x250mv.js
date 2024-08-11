7a44482b753b0a8929b072c9a210c56e22d97a6c
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-08-11 21:08:36 +0700

    Create 300x250mv.js

---
+++ gan/300x250mv.js
@@ -0,0 +1,10 @@
+atOptions = {
+		'key' : 'c9132b1546f661fc9db98025b3a7cd47',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/c9132b1546f661fc9db98025b3a7cd47/invoke.js"></scr' + 'ipt>');
