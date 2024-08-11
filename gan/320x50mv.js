e6cb65cd77cc3db9c7b0c1fe2b02a8fc0f4e427b
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-08-11 20:48:55 +0700

    Create 320x50mv.js

---
+++ gan/320x50mv.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '6f46d3f11fdc3f26732f5524969d00b7',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/6f46d3f11fdc3f26732f5524969d00b7/invoke.js"></scr' + 'ipt>');
