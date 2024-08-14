96008f9087338854fc5dcb530ddd98103793c391
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-08-14 08:18:17 +0700

    Update 728x90mv.js

--- gan/728x90mv.js
+++ gan/728x90mv.js
@@ -1,1 +1,9 @@
+atOptions = {
+		'key' : 'b9e1afe3b3ca12ef966acffdc90a1ea7',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
 
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/b9e1afe3b3ca12ef966acffdc90a1ea7/invoke.js"></scr' + 'ipt>');
