0a915d61b8d51cf1f79af65ccdd7f8b070a73954
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-09-25 17:05:08 +0700

    Update 728x90mv.js

--- gan/728x90mv.js
+++ gan/728x90mv.js
@@ -1,9 +1,19 @@
+// atOptions = {
+// 		'key' : 'b9e1afe3b3ca12ef966acffdc90a1ea7',
+// 		'format' : 'iframe',
+// 		'height' : 90,
+// 		'width' : 728,
+// 		'params' : {}
+// 	};
+
+// document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/b9e1afe3b3ca12ef966acffdc90a1ea7/invoke.js"></scr' + 'ipt>');
+
 atOptions = {
-		'key' : 'b9e1afe3b3ca12ef966acffdc90a1ea7',
+		'key' : '596b0ce49e62678f50d8bea676e75766',
 		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
+		'height' : 60,
+		'width' : 468,
 		'params' : {}
 	};
 
-document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/b9e1afe3b3ca12ef966acffdc90a1ea7/invoke.js"></scr' + 'ipt>');
+ document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/596b0ce49e62678f50d8bea676e75766/invoke.js"></scr' + 'ipt>');
