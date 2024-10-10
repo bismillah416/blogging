2004f9f2057109c4f2809802bbaf44b9e63148e0
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-10-10 21:31:54 +0700

    Update dir_blogspot.js

--- gan/dir_blogspot.js
+++ gan/dir_blogspot.js
@@ -1,13 +1,51 @@
-$(document).ready(function(){
+ if(!localStorage['akutampan']){
+    getdir('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','satu');
+  }
+
+  if(localStorage['akutampan']=='satu')
+  {
+     getdir('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','dua');
+  }else if(localStorage['akutampan']=='dua')
+  {
+    getdir('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','tiga');
+  }
+
+
+
+function getdir(url,cache)
+{
+  
+  $(document).ready(function(){
               $('body').addClass('xepo_ads');
          });
 
        $(document).on('click','.xepo_ads',function(e){
-         document.body.classList.remove('xepo_ads');
-         window.open('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','_blank');
+         localStorage['akutampan'] = cache;
+         if(localStorage['akutampan']=='tiga')
+         {
+            document.body.classList.remove('xepo_ads');
+         }
+          if(localStorage['akutampan']!='tiga')
+         {
+          window.open(url,'_blank');
+          location.reload();
+         }
+         
        });
 
      $(document).on('click','img',function(e){
-         document.body.classList.remove('xepo_ads');
-         window.open('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','_blank');    
+         localStorage['akutampan'] = cache;
+         if(localStorage['akutampan']=='tiga')
+         {
+            document.body.classList.remove('xepo_ads');
+         }
+        if(localStorage['akutampan']!='tiga')
+         {
+          window.open(url,'_blank');
+          location.reload();
+         }
+         
        });
+
+
+   }
