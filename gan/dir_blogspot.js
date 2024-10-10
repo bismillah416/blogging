81cc063ea743db8cfb983ef0f255ed7c203b52f2
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-10-10 09:27:53 +0700

    Create dir_blogspot.js

---
+++ gan/dir_blogspot.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+              $('body').addClass('xepo_ads');
+         });
+
+       $(document).on('click','.xepo_ads',function(e){
+         document.body.classList.remove('xepo_ads');
+         window.open('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','_blank');
+       });
+
+     $(document).on('click','img',function(e){
+         document.body.classList.remove('xepo_ads');
+         window.open('https://controlaffliction.com/j4q8pc0v?key=becf874ddf28e7a87c797bd5128a5314','_blank');    
+       });
