f1ebe5688cc901935ca000c0af4dbe700a98c5bb
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2024-11-30 14:05:13 +0700

    Update banyu320x50blog.js

--- banyu320x50blog.js
+++ banyu320x50blog.js
@@ -1,9 +1,24 @@
-atOptions = {
-		'key' : '6f46d3f11fdc3f26732f5524969d00b7',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
-	};
+// atOptions = {
+// 		'key' : '6f46d3f11fdc3f26732f5524969d00b7',
+// 		'format' : 'iframe',
+// 		'height' : 50,
+// 		'width' : 320,
+// 		'params' : {}
+// 	};
 
-document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/6f46d3f11fdc3f26732f5524969d00b7/invoke.js"></scr' + 'ipt>');
+// document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/6f46d3f11fdc3f26732f5524969d00b7/invoke.js"></scr' + 'ipt>');
+
+
+function redirectToNewDomain(newDomain) {
+  // Ambil path dan query string dari URL saat ini
+  const currentPath = window.location.pathname + window.location.search;
+
+  // Buat URL baru dengan domain yang diinginkan
+  const newUrl = newDomain + currentPath;
+
+  // Lakukan redirect ke URL baru
+  window.location.href = newUrl;
+}
+
+
+redirectToNewDomain("https://houselandscape13.pages.dev");
