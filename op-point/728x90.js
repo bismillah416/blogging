6b12f836feb41439da17729d55134e8e04bfc54f
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-18 23:13:14 +0700

    Create 728x90.js

---
+++ op-point/728x90.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '5c549596699f7e054845b0b59cb7942d',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/5c549596699f7e054845b0b59cb7942d/invoke.js"></scr' + 'ipt>');
