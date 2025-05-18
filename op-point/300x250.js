f5a340e6b316d7d568f59673db11dc4cd8fc53cd
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-18 22:58:23 +0700

    Create 300x250.js

---
+++ op-point/300x250.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : 'a01b2bd5a9ae535fffe3860b8e84a3e8',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/a01b2bd5a9ae535fffe3860b8e84a3e8/invoke.js"></scr' + 'ipt>');
