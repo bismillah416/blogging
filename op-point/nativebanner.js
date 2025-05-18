865b3cae3d99dcaae95023492be7f01cbc26b823
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-18 23:37:20 +0700

    Create nativebanner.js

---
+++ op-point/nativebanner.js
@@ -0,0 +1,2 @@
+document.write('<script async="async" data-cfasync="false" src="//controlaffliction.com/7c77b5a026a3a290456a3daa0ec161ee/invoke.js"></script>');
+document.write('<div id="container-7c77b5a026a3a290456a3daa0ec161ee"></div>');
