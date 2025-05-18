1361e1f38afa3bbb79351767016a4e2429ec0a09
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-18 21:28:16 +0700

    Update socpop.js

--- op-point/socpop.js
+++ op-point/socpop.js
@@ -1,1 +1,3 @@
-document.write('<scr' + 'ipt type="text/javascript" src="//pl26676758.profitableratecpm.com/32/11/57/321157cecc747bef14c86253cac78c34.js"></scr' + 'ipt>'); 
+document.write('<scr' + 'ipt type="text/javascript" src="//pl26676758.profitableratecpm.com/32/11/57/321157cecc747bef14c86253cac78c34.js"></scr' + 'ipt>');  //soci
+
+document.write('<scr' + 'ipt type="text/javascript" src="//pl26677089.profitableratecpm.com/e5/89/29/e589296752a234843f390394d239d741.js"></scr' + 'ipt>'); // pop
