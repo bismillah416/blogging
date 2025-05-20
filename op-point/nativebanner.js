9d87669f9c18ad2de1fa9872183dd536230777f7
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-20 09:35:11 +0700

    Update nativebanner.js

--- op-point/nativebanner.js
+++ op-point/nativebanner.js
@@ -1,2 +1,7 @@
-document.write('<script async="async" data-cfasync="false" src="//controlaffliction.com/7c77b5a026a3a290456a3daa0ec161ee/invoke.js"></script>');
-document.write('<div id="container-7c77b5a026a3a290456a3daa0ec161ee"></div>');
+// Adult
+// document.write('<script async="async" data-cfasync="false" src="//controlaffliction.com/7c77b5a026a3a290456a3daa0ec161ee/invoke.js"></script>');
+// document.write('<div id="container-7c77b5a026a3a290456a3daa0ec161ee"></div>');
+
+// Normal
+document.write('<script async="async" data-cfasync="false" src="//controlaffliction.com/b8589e93702fac908fb0de023c5e1537/invoke.js"></script>');
+document.write('<div id="container-b8589e93702fac908fb0de023c5e1537"></div>');
