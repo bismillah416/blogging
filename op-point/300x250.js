999f0b378773762e4d15b88d1a117307058355d0
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-20 09:38:20 +0700

    Update 300x250.js

--- op-point/300x250.js
+++ op-point/300x250.js
@@ -1,9 +1,9 @@
 atOptions = {
-		'key' : 'a01b2bd5a9ae535fffe3860b8e84a3e8',
+		'key' : '1a72e4274260daca5b158baa4a4b2871',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
 
-document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/a01b2bd5a9ae535fffe3860b8e84a3e8/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/1a72e4274260daca5b158baa4a4b2871/invoke.js"></scr' + 'ipt>');
