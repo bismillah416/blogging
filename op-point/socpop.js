6a084f73ea82ac347d312884a1fdec91ff9eddcc
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-20 09:29:37 +0700

    Update socpop.js

--- op-point/socpop.js
+++ op-point/socpop.js
@@ -1,3 +1,7 @@
-document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/dd/ec/67/ddec67997c0db3d2cda7386934249e9b.js"></scr' + 'ipt>');  //soci
+// ---- Adult
+// document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/dd/ec/67/ddec67997c0db3d2cda7386934249e9b.js"></scr' + 'ipt>');  //soci
+// document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/d3/d0/7c/d3d07c6621e1dfcc0e098705fb2f38a9.js"></scr' + 'ipt>'); // pop
 
-document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/d3/d0/7c/d3d07c6621e1dfcc0e098705fb2f38a9.js"></scr' + 'ipt>'); // pop
+// ---------- Normal
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/72/26/2c/72262ca62ca38bdf83dc8aa26c11e960.js"></scr' + 'ipt>');  //soci
+document.write('<scr' + 'ipt type="text/javascript" src="//controlaffliction.com/06/69/1d/06691dbc783940949ba22dc9542003b5.js"></scr' + 'ipt>'); // pop
