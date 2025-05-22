06c02129f385794fefa6cbf7395ace51902827c2
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-22 21:33:01 +0700

    Create socpop.js

---
+++ gan/socpop.js
@@ -0,0 +1,2 @@
+document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/d3/e5/a9/d3e5a9bb1f13c778562b21557904e784.js"></scr' + 'ipt>'); // soci
+document.write('<scr' + 'ipt type="text/javascript" src="//perilastronaut.com/9a/3b/67/9a3b6748d0bc84e71242ef1d017831bd.js"></scr' + 'ipt>'); // popo
