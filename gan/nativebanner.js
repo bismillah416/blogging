c9354e7cc40ebf2fb7d372e041d743dff23f0d04
Jaga Pandanganmu <142307313+bismillah416@users.noreply.github.com>
2025-05-22 21:34:40 +0700

    Update nativebanner.js

--- gan/nativebanner.js
+++ gan/nativebanner.js
@@ -1,2 +1,2 @@
-// document.write('<script async="async" data-cfasync="false" src="//perilastronaut.com/a5b24110d92b0eca6ec15190d1c72ce9/invoke.js"></script>');
-// document.write('<div id="container-a5b24110d92b0eca6ec15190d1c72ce9"></div>');
+document.write('<script async="async" data-cfasync="false" src="//perilastronaut.com/a5b24110d92b0eca6ec15190d1c72ce9/invoke.js"></script>');
+document.write('<div id="container-a5b24110d92b0eca6ec15190d1c72ce9"></div>');
